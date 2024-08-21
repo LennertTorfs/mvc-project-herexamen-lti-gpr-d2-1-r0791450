6fcebe5e255a3f5638c118fa5a9af3ce43862b6d
LennertTorfs <r0791450@student.thomasmore.be>
2024-08-21 23:21:15 +0200

    added ef

---
+++ MVC_Project_Herexamen/MVC_Project_Herexamen/Data/MVCProjectContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Project_Herexamen.Data
+{
+    public class MVCProjectContext :DbContext
+    {
+        public MVCProjectContext(DbContextOptions<MVCProjectContext> options) : base(options) { }
+
+
+
+    }
+}
--- MVC_Project_Herexamen/MVC_Project_Herexamen/Program.cs
+++ MVC_Project_Herexamen/MVC_Project_Herexamen/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MVC_Project_Herexamen.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MVCProjectContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 
 var app = builder.Build();
 
