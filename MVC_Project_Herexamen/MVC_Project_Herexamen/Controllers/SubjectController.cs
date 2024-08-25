03deff6a438c2bd1372e78f759a902223812bf93
LennertTorfs <r0791450@student.thomasmore.be>
2024-08-25 23:02:31 +0200

    vakkenbeheer

---
+++ MVC_Project_Herexamen/MVC_Project_Herexamen/Controllers/SubjectController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Project_Herexamen.Models;
+using MVC_Project_Herexamen.Data;
+
+public class SubjectController : Controller
+{
+    private readonly MVCProjectContext _context;
+
+    public SubjectController(MVCProjectContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index(bool showInactive = false)
+    {
+        var subjects = _context.Subjects
+            .Where(s => showInactive || !s.Deleted)
+            .ToList();
+
+        ViewBag.ShowInactive = showInactive;
+        return View(subjects);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Subject subject)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Subjects.Add(subject);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(subject);
+    }
+
+    public IActionResult Edit(int id)
+    {
+        var subject = _context.Subjects.Find(id);
+        if (subject == null)
+        {
+            return NotFound();
+        }
+        return View(subject);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Subject subject)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Subjects.Update(subject);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(subject);
+    }
+
+    [HttpPost]
+    public IActionResult ToggleActive(int id)
+    {
+        var subject = _context.Subjects.Find(id);
+        if (subject != null)
+        {
+            subject.Deleted = !subject.Deleted;
+            _context.SaveChanges();
+        }
+        return RedirectToAction("Index");
+    }
+}
